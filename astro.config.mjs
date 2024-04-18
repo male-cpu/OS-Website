bd4bae9d5d140a3bfde7b59efc93841303a90c75
MaleCPU <nedicobrad@gmail.com>
2024-04-18 03:35:10 +0200

    Changing thumbnail

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    meta: {
+        // Title of the website
+        title: 'Old School BJJ',
+        // Description of the website
+        description: 'The School for Grappling and BJJ',
+        // Favicon configuration
+        favicon: 'src/images/Old School 500x500 Logo.png', // or 'path/to/favicon.png'
+     }
+});
